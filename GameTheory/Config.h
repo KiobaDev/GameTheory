2c9bef6c60b5b75404e13cf131cb80fac65c7d98
Adam Grajdek <avi19990@gmail.com>
2021-10-16 18:36:13 +0200

    Init

---
+++ GameTheory/Config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//General
+enum class Pawn { none, black, white };
+#define BOARD_SIZE 24
+
+//Second method
+#define GAME_COUNT 1000
+#define NUM_OF_SIMULATIONS 100
+#define NUM_OF_STATES 24 * 24 * 24
