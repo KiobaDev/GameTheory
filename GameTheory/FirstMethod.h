db29e9c346c4ac6b3a71bbe4c50c24143c97b6c9
Adam Grajdek <avi19990@gmail.com>
2021-10-17 20:31:56 +0200

    Implemented defeat by no possible moves, simulation of multiple games and evaluating average turn counts and possible moves per turn

--- GameTheory/FirstMethod.h
+++ GameTheory/FirstMethod.h
@@ -127,115 +127,111 @@ Pawn getOpponent(Pawn playerTurn)
 void calculateGameComplexityM1()
 {
 	srand(time(0));
-	Pawn board[BOARD_SIZE] = {};
 
-	Pawn playerTurn = Pawn::black;
-	bool isGameWon = false;
+	std::vector<int> turnCounts;
+	std::vector<double> averageMovesPerTurn;
 
-	std::vector<int> possibleMovesInTurns;
-
-	int turn = 0;
-
-	while (!isGameWon)
+	for (int g = 0; g < GAME_COUNT; ++g)
 	{
-		++turn;
+		Pawn board[BOARD_SIZE] = {};
 
-		if (turn <= 18)
+		Pawn playerTurn = Pawn::black;
+		bool isGameWon = false;
+
+		std::vector<int> possibleMovesInTurns;
+
+		int turn = 0;
+
+		while (!isGameWon)
 		{
-			//Phase 1
-			std::vector<int> freeSpaces = findFreeSpaces(board);
-			int space = freeSpaces[rand() % freeSpaces.size()];
+			++turn;
 
-			int possibleRemoves = 0;
-			for (int i = 0; i < freeSpaces.size(); ++i)
-				if (willPawnBeInLineAfterInsert(board, playerTurn, freeSpaces[i]))
-					possibleRemoves += findPlayerPawns(board, getOpponent(playerTurn)).size(); //TODO: dont remove pawns in line
-
-			board[space] = playerTurn;
-
-			if (isPawnInLine(board, space))
+			if (turn <= 18)
 			{
-				std::vector<int> oponentPawns = findPlayerPawns(board, getOpponent(playerTurn));
-				space = oponentPawns[rand() % oponentPawns.size()];
+				//Phase 1
+				std::vector<int> freeSpaces = findFreeSpaces(board);
 
-				board[space] = Pawn::none;
+				if (freeSpaces.size() == 0)
+					break;
+
+				int space = freeSpaces[rand() % freeSpaces.size()];
+
+				int possibleRemoves = 0;
+				for (int i = 0; i < freeSpaces.size(); ++i)
+					if (willPawnBeInLineAfterInsert(board, playerTurn, freeSpaces[i]))
+						possibleRemoves += findPlayerPawns(board, getOpponent(playerTurn)).size();
+
+				board[space] = playerTurn;
+
+				if (isPawnInLine(board, space))
+				{
+					std::vector<int> oponentPawns = findPlayerPawns(board, getOpponent(playerTurn));
+					space = oponentPawns[rand() % oponentPawns.size()];
+
+					board[space] = Pawn::none;
+				}
+
+				possibleMovesInTurns.push_back(freeSpaces.size() + possibleRemoves);
 			}
-
-			possibleMovesInTurns.push_back(freeSpaces.size() + possibleRemoves);
-		}
-		else
-		{
-			std::vector<std::pair<int, int>> possibleCurrentMoves;
-
-			if (findPlayerPawns(board, playerTurn).size() > 3)
-				possibleCurrentMoves = findPossibleMovesInSecondPhase(board, playerTurn);
 			else
-				possibleCurrentMoves = findPossibleMovesInThirdPhase(board, playerTurn);
-
-			std::pair<int, int> move = possibleCurrentMoves[rand() % possibleCurrentMoves.size()];
-
-			int possibleRemoves = 0;
-			for (int i = 0; i < possibleCurrentMoves.size(); ++i)
-				if (willPawnBeInLineAfterMove(board, playerTurn, possibleCurrentMoves[i]))
-					possibleRemoves += findPlayerPawns(board, getOpponent(playerTurn)).size(); //TODO: dont remove pawns in line
-
-			board[move.first] = Pawn::none;
-			board[move.second] = playerTurn;
-
-			if (isPawnInLine(board, move.second))
 			{
-				std::vector<int> oponentPawns = findPlayerPawns(board, getOpponent(playerTurn));
-				int space = oponentPawns[rand() % oponentPawns.size()];
+				std::vector<std::pair<int, int>> possibleCurrentMoves;
 
-				board[space] = Pawn::none;
+				if (findPlayerPawns(board, playerTurn).size() > 3)
+					possibleCurrentMoves = findPossibleMovesInSecondPhase(board, playerTurn);
+				else
+					possibleCurrentMoves = findPossibleMovesInThirdPhase(board, playerTurn);
+
+				if (possibleCurrentMoves.size() == 0)
+					break;
+
+				std::pair<int, int> move = possibleCurrentMoves[rand() % possibleCurrentMoves.size()];
+
+				int possibleRemoves = 0;
+				for (int i = 0; i < possibleCurrentMoves.size(); ++i)
+					if (willPawnBeInLineAfterMove(board, playerTurn, possibleCurrentMoves[i]))
+						possibleRemoves += findPlayerPawns(board, getOpponent(playerTurn)).size();
+
+				board[move.first] = Pawn::none;
+				board[move.second] = playerTurn;
+
+				if (isPawnInLine(board, move.second))
+				{
+					std::vector<int> oponentPawns = findPlayerPawns(board, getOpponent(playerTurn));
+					int space = oponentPawns[rand() % oponentPawns.size()];
+
+					board[space] = Pawn::none;
+				}
+
+				possibleMovesInTurns.push_back(possibleCurrentMoves.size() + possibleRemoves);
+
+				if (findPlayerPawns(board, getOpponent(playerTurn)).size() == 2)
+					isGameWon = true;
 			}
 
-			possibleMovesInTurns.push_back(possibleCurrentMoves.size() + possibleRemoves);
+			playerTurn = getOpponent(playerTurn);
 
-			if (findPlayerPawns(board, getOpponent(playerTurn)).size() == 2)
-				isGameWon = true;
+			if (turn > 1000)
+				break;
 		}
 
-		playerTurn = getOpponent(playerTurn);
-		
-		int test[BOARD_SIZE] = {};
-		for (int i = 0; i < BOARD_SIZE; ++i)
-			test[i] = static_cast<int>(board[i]);
+		int sum = 0;
+		for (int moves : possibleMovesInTurns)
+			sum += moves;
 
-		#pragma region abomination
-				printf((test[0] == 0 ? "\x1B[32m%d     " : (test[0] == 1 ? "\x1B[31m%d     " : "\x1B[34m%d     ")), test[0]);
-				printf((test[1] == 0 ? "\x1B[32m%d     " : (test[1] == 1 ? "\x1B[31m%d     " : "\x1B[34m%d     ")), test[1]);
-				printf((test[2] == 0 ? "\x1B[32m%d\n" : (test[2] == 1 ? "\x1B[31m%d\n" : "\x1B[34m%d\n")), test[2]);
-				printf((test[3] == 0 ? "  \x1B[32m%d   " : (test[3] == 1 ? "  \x1B[31m%d   " : "  \x1B[34m%d   ")), test[3]);
-				printf((test[4] == 0 ? "\x1B[32m%d   " : (test[4] == 1 ? "\x1B[31m%d   " : "\x1B[34m%d   ")), test[4]);
-				printf((test[5] == 0 ? "\x1B[32m%d  \n" : (test[5] == 1 ? "\x1B[31m%d  \n" : "\x1B[34m%d  \n")), test[5]);
-				printf((test[6] == 0 ? "    \x1B[32m%d " : (test[6] == 1 ? "    \x1B[31m%d " : "    \x1B[34m%d ")), test[6]);
-				printf((test[7] == 0 ? "\x1B[32m%d " : (test[7] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[7]);
-				printf((test[8] == 0 ? "\x1B[32m%d    \n" : (test[8] == 1 ? "\x1B[31m%d    \n" : "\x1B[34m%d    \n")), test[8]);
-				printf((test[9] == 0 ? "\x1B[32m%d " : (test[9] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[9]);
-				printf((test[10] == 0 ? "\x1B[32m%d " : (test[10] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[10]);
-				printf((test[11] == 0 ? "\x1B[32m%d   " : (test[11] == 1 ? "\x1B[31m%d   " : "\x1B[34m%d   ")), test[11]);
-				printf((test[12] == 0 ? "\x1B[32m%d " : (test[12] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[12]);
-				printf((test[13] == 0 ? "\x1B[32m%d " : (test[13] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[13]);
-				printf((test[14] == 0 ? "\x1B[32m%d\n" : (test[14] == 1 ? "\x1B[31m%d\n" : "\x1B[34m%d\n")), test[14]);
-				printf((test[15] == 0 ? "    \x1B[32m%d " : (test[15] == 1 ? "    \x1B[31m%d " : "    \x1B[34m%d ")), test[15]);
-				printf((test[16] == 0 ? "\x1B[32m%d " : (test[16] == 1 ? "\x1B[31m%d " : "\x1B[34m%d ")), test[16]);
-				printf((test[17] == 0 ? "\x1B[32m%d  \n" : (test[17] == 1 ? "\x1B[31m%d  \n" : "\x1B[34m%d  \n")), test[17]);
-				printf((test[18] == 0 ? "  \x1B[32m%d   " : (test[18] == 1 ? "  \x1B[31m%d   " : "  \x1B[34m%d   ")), test[18]);
-				printf((test[19] == 0 ? "\x1B[32m%d   " : (test[19] == 1 ? "\x1B[31m%d   " : "\x1B[34m%d   ")), test[19]);
-				printf((test[20] == 0 ? "\x1B[32m%d  \n" : (test[20] == 1 ? "\x1B[31m%d  \n" : "\x1B[34m%d  \n")), test[20]);
-				printf((test[21] == 0 ? "\x1B[32m%d     " : (test[21] == 1 ? "\x1B[31m%d     " : "\x1B[34m%d     ")), test[21]);
-				printf((test[22] == 0 ? "\x1B[32m%d     " : (test[22] == 1 ? "\x1B[31m%d     " : "\x1B[34m%d     ")), test[22]);
-				printf((test[23] == 0 ? "\x1B[32m%d\n" : (test[23] == 1 ? "\x1B[31m%d\n" : "\x1B[34m%d\n")), test[23]);
-				printf("Turn: %d Player: %d Blue: %d Red: %d\n", turn, static_cast<int>(getOpponent(playerTurn)), findPlayerPawns(board, Pawn::white).size(), findPlayerPawns(board, Pawn::black).size());
-				printf("--------------\n");
-		#pragma endregion
+		double average = double(sum) / possibleMovesInTurns.size();
 
-		getchar();
+		turnCounts.push_back(turn);
+		averageMovesPerTurn.push_back(average);
 	}
 
-	std::cout << "Game Over" << std::endl;
-	for (int movesInTurn : possibleMovesInTurns)
-		std::cout << movesInTurn << " ";
-	std::cout << std::endl;
+	int averageTurns = 0;
+	for (int turns : turnCounts)
+		averageTurns += turns;
+
+	double averageMoves = 0;
+	for (double moves : averageMovesPerTurn)
+		averageMoves += moves;
+
+	std::wcout << "Srednia ilosc tur: " << averageTurns / turnCounts.size() << " Srednia ilosc rochow: " << averageMoves / averageMovesPerTurn.size() << std::endl;
 }
