73e3845be675f0bed019fbb206498f48e24765ad
Adam Grajdek <avi19990@gmail.com>
2021-10-18 20:23:52 +0200

    Fixed number of states

--- GameTheory/Config.h
+++ GameTheory/Config.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <cmath>
+
 //General
 enum class Pawn { none, black, white };
 #define BOARD_SIZE 24
 
 //Second method
-#define GAME_COUNT 1000
+#define GAME_COUNT 100
 #define NUM_OF_SIMULATIONS 100
-#define NUM_OF_STATES 24 * 24 * 24
+#define NUM_OF_STATES pow(3, 24)
