9b51dad023825649667beef01d802b01f361a81e
Adam Grajdek <avi19990@gmail.com>
2021-10-29 19:38:28 +0200

    Implemented minimax and bestMove

---
+++ GameTheory/NMMMinimax.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <algorithm>
+
+#include "NMMTree.h"
+#include "NMMEvaluation.h"
+
+namespace NMM
+{
+	int minimax(NMM::Node* parent, NMM::Node* tree, int depth, int player, bool maximalizedPlayer)
+	{
+		if (depth == 0 || isPlayerWinning(tree->board, player))
+			return NMM::NMMEvaluation(parent->board, tree->board, player);
+		
+		if (maximalizedPlayer)
+		{
+			int maxEval = INT_MIN;
+
+			for (NMM::Node* child : tree->children)
+			{
+				int eval = minimax(tree, child, depth - 1, tree->board.getOpponent(player), false);
+				maxEval = std::max(maxEval, eval);
+			}
+
+			return maxEval;
+		}
+		else
+		{
+			int minEval = INT_MAX;
+
+			for (NMM::Node* child : tree->children)
+			{
+				int eval = minimax(tree, child, depth - 1, tree->board.getOpponent(player), true);
+				minEval = std::min(minEval, eval);
+			}
+
+			return minEval;
+		}
+	}
+
+	NMM::BoardState bestMove(NMM::Node* tree, int player, int depth)
+	{
+		int maxEval = INT_MIN;
+		NMM::BoardState bestState;
+
+		for (NMM::Node* child : tree->children)
+		{
+			int eval = minimax(tree, child, depth - 1, tree->board.getOpponent(player), false);
+
+			if (eval > maxEval)
+			{
+				bestState = child->board;
+				maxEval = eval;
+			}
+		}
+
+		return bestState;
+	}
+}
